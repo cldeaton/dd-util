4d3616dfc2519408e831aa37d613f87e16cc39c5
Chris Deaton <chrisdeaton@Chriss-MacBook-Pro.local>
2018-05-02 11:40:01 -0400

    refactored method

--- dd-shiftby.js
+++ dd-shiftby.js
@@ -2,23 +2,14 @@
 
 Array.prototype.shiftBy = function (movements){
   movements = movements % this.length;
-
-  var length = this.length-1;
+  if (movements < 0){
+    movements = movements + this.length;
+  }
   var spliced = this.length-movements;
-  var negSplice = Math.abs(movements);
 
-  if (negSplice > length){
-    negSplice = negStart + movements;
-  }
+  var back = this.slice(spliced);
+  var front  = this.slice(0, spliced)
+  var altArray = back.concat(front);
 
-  if(movements < 0){
-    front = this.slice(0, negSplice);
-    back = this.slice(negSplice, this.length);
-    var altArray = back.concat(front);
-  } else {
-    var back = this.slice(spliced);
-    var front  = this.slice(0, spliced)
-    var altArray = back.concat(front);
-  }
   return altArray;
 }
